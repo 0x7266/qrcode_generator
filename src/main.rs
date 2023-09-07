c717af5fb8ada6ae0c30d52a9074b8dec00fe925
unknown <rrrr0ffff@gmail.com>
2023-09-07 13:30:52 -0300

    rendering as unicode

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
-use qrcode::{render::svg, QrCode};
+use qrcode::{render::unicode, QrCode};
 use std::env;
 
 fn main() {
     let url = if env::args().count() == 2 {
         env::args().nth(1).unwrap()
     } else {
-        panic!("Please enter a url")
+        panic!("Please provide a url")
     };
     match QrCode::new(url.as_bytes()) {
         Ok(bytes) => {
-            let qrcode = bytes
-                .render()
-                .dark_color(svg::Color("#800000"))
-                .light_color(svg::Color("#ffff80"))
-                .min_dimensions(200, 200)
-                .build();
+            let qrcode = bytes.render::<unicode::Dense1x2>().build();
             println!("{}", qrcode);
         }
         Err(err) => println!("{}", err),
